ada6743d42481143c52f81d3e602a2c19a19e26c
dhika090 <nyes6953@gmail.com>
2023-07-13 16:30:43 +0700

    revisi layout modal

--- src/views/board-view.vue
+++ src/views/board-view.vue
@@ -418,7 +418,7 @@ export default {
         type: 'payRent',
         currTile,
       })
-      const msg = `${this.currPLayer.name} pay $${amount} rent, to ${currTile.owner.name}`
+      const msg = `${this.currPLayer.name} membayar $${amount} sewa, ke ${currTile.owner.name}`
       this.showUpdate(msg, 7000)
     },
     openBuyHouseModal(name) {
