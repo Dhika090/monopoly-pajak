72e4cc751f4abbe001f0dd4b20e8298cf902a9ee
dhika090 <nyes6953@gmail.com>
2023-07-21 15:29:53 +0700

    changes package

--- vite.config.js
+++ vite.config.js
@@ -14,15 +14,7 @@ export default defineConfig({
   base: '/monopoly-pajak/',
   // build: '../backend/public',
   build: {
-    rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
-        }
-    }
+    chunkSizeWarningLimit: 3600,
   },
   emptyOutDir: true,
 })
